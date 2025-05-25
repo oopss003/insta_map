46299dbf3ea3733c21948cc07e6e7ca9dcdb3dbd
ahepf8271 <ahepf8271@gmail.com>
2025-05-25 23:37:06 +0900

    폴더 구조 정리 & 서버리스 geocode 함수 적용

--- api/geocode.js
+++ api/geocode.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
+const CLIENT_ID     = 'qpbrmxiff4';
+const CLIENT_SECRET = 'mymd8vVOsm59IHlwczl0MDyOEFDiI3D4kpRIgaUW';
+
 export default async function handler(req, res) {
   const { query } = req.query;
-
-  const CLIENT_ID     = 'qpbrmxiff4';
-  const CLIENT_SECRET = 'mymd8vVOsm59IHlwczl0MDyOEFDiI3D4kpRIgaUW';
+  if (!query) return res.status(400).json({ error: 'query 누락' });
 
   try {
     const resp = await axios.get(
       `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodeURIComponent(query)}`,
-      {
-        headers: {
+      { headers: {
           'X-NCP-APIGW-API-KEY-ID': CLIENT_ID,
           'X-NCP-APIGW-API-KEY'   : CLIENT_SECRET
-        }
-      }
+        } }
     );
     res.status(200).json(resp.data);
   } catch (err) {
